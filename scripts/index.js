9bb8f055d9a15a895ef5a7d7d6a01c08640ae263
Murat Palvanov <palvanoff.muratpalvanov@yandex.ru>
2024-03-27 13:16:42 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCardElement(cardData, deleteCallback)  {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
+    let elementImage = cardElement.querySelector('.card__image');
+    let elementTitle = cardElement.querySelector('.card__title');
+    let likeButton = cardElement.querySelector('.card__like-button');
+    let deleteButton = cardElement.querySelector('.card__delete-button');
+    elementImage.src = cardData.link;
+    elementImage.alt = cardData.name;
+    elementTitle.textContent = cardData.name;
+
+    deleteButton.addEventListener('click', function() {
+        // btn = deleteButton.closest('.places__item');
+        // btn.remove();
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElem) {
+    cardElem.remove();
+};
+
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(item) {
+    const cardElem = createCardElement(item, deleteCard);
+    placesList.append(cardElem);
+});
+
+
