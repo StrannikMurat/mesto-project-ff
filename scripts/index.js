c67321001d2dc84df3fb52f6afd0980a35957b05
Murat Palvanov <palvanoff.muratpalvanov@yandex.ru>
2024-03-27 21:37:14 +0300

    2

--- scripts/index.js
+++ scripts/index.js
@@ -6,17 +6,15 @@ const placesList = document.querySelector('.places__list');
 function createCardElement(cardData, deleteCallback)  {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-    let elementImage = cardElement.querySelector('.card__image');
-    let elementTitle = cardElement.querySelector('.card__title');
-    let likeButton = cardElement.querySelector('.card__like-button');
-    let deleteButton = cardElement.querySelector('.card__delete-button');
+    const elementImage = cardElement.querySelector('.card__image');
+    const elementTitle = cardElement.querySelector('.card__title');
+    const likeButton = cardElement.querySelector('.card__like-button');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
     elementImage.src = cardData.link;
     elementImage.alt = cardData.name;
     elementTitle.textContent = cardData.name;
 
     deleteButton.addEventListener('click', function() {
-        // btn = deleteButton.closest('.places__item');
-        // btn.remove();
         deleteCard(cardElement);
     });
 
